2d2cb909e708c9b83f30d14d037ad009e8cb5f25
Rico <r@rico.wtf>
2023-07-19 15:21:09 +0200

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -4,11 +4,18 @@ function(instance, properties, context) {
     let locale = properties.locale;
     let compactDisplay = properties.compactDisplay;
     if (compactDisplay == "narrow") {let compactDisplay = "short";};
+        
+    let options;
+    if (!properties.advanced_options) {
+        options = {
+            notation: "compact",
+            compactDisplay: compactDisplay,
+        };
+    } else {
+        const advanced_options = JSON.parse(properties.advanced_options);
+        options = advanced_options;
+    }
     
-    let options = {
-		notation: "compact",
-        compactDisplay: compactDisplay,
-    };
 	let formatter = new Intl.NumberFormat( [locale, "en-US"], options);
 
     let n = formatter.format(number);
